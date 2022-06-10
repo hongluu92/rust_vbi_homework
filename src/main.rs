6731796adb7cc59446d7eafdf5468860d69c278b
honglm <honglm1011@gmail.com>
2022-06-10 18:04:28 +0700

    Check sublist contain: Add case sub_list.len > origin_list.len

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ fn main() {
     println!("sub_arr_3 :{}",check_containt(&mut org_arr, &mut [7,8,9]));
     println!("sub_arr_4 :{}",check_containt(&mut org_arr, &mut [1,2]));
     println!("sub_arr_5 :{}",check_containt(&mut org_arr, &mut [2,3,4,6]));
+    println!("sub_arr_6 :{}",check_containt(&mut org_arr, &mut [2,3,4,6,1,1,1,1,1,11,1,1,1,1,1,1,1,1,11]));
     
     // Bai 2
     println!("=== COUNT ME ==="); 
@@ -38,7 +39,9 @@ fn main() {
 fn check_containt( org_arr: &mut [i32], sub_arr: &mut [i32]) -> bool{
     let org_length = org_arr.len();
     let sub_length = sub_arr.len();
-    
+    if org_length < sub_length {
+        return false;
+    }
     for i in 0..(org_length - sub_length + 1) {
         for j in 0..(sub_length ) {
             if org_arr[i+j] != sub_arr[j] {
